ed8e912fbe146f83c4e111656245b10983eb8c86
owl <andrasbalint13@protonmail.com>
2024-07-05 18:21:29 +0100

    Fixed points

--- Tournament.py
+++ Tournament.py
@@ -1,49 +1,44 @@
 import random
 
+
 class Tournament:
+    p1points = 0
+    p2points = 0
 
-
-    #def __init__(self):
-    #self.run_tournament
-
+    # def __init__(self):
+    # self.run_tournament
     def run_tournament(self):
         x = 2
-
         amount_of_rounds = 10
         for y in range(amount_of_rounds):
             x = self.tit_for_tat(x)
-            p1 = x
+            player1 = x
             x = self.random_answer()
-            p2 = x
-            print(p1, p2)
+            player2 = x
+            print(player1, player2)
 
-            self.calculate_points(p1, p2)
+            self.calculate_points(player1, player2)
+            #print(self.p1points, self.p2points)
 
+        print(self.p1points, self.p2points)
 
     def calculate_points(self, p1, p2):
-            global p1points
-            global p2points
 
+        if p1 == 0 and p2 == 0:
+            self.p1points += 1
+            self.p2points += 1
 
+        if p1 == 1 and p2 == 0:
+            self.p2points += 10
 
-            if p1 == 0 & p2 == 0:
-                p1points += 1
-                p2points += 1
+        if p1 == 0 and p2 == 1:
+            self.p1points += 10
 
-            if p1 == 1 & p2 == 0:
-                p1points += 0
-                p2points += 10
+        if p1 == 1 and p2 == 1:
+            self.p1points += 3
+            self.p2points += 3
 
-            if p1 == 0 & p2 == 1:
-                p1points += 10
-                p2points += 0
-
-            if p1 == 1 & p2 == 1:
-                p1points += 3
-                p2points += 3
-
-
-
+        print(self.p1points, self.p2points)
 
     def tit_for_tat(self, x):
 
@@ -55,7 +50,6 @@ class Tournament:
 
         if x == 2:
             return 1
-
 
     def random_answer(self):
         return random.randint(0, 1)
